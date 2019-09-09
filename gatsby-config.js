97fac63c1d49e08428065bc1c8a1d46788003fa4
Rafael Villarreal <rvillarreal416@gmail.com>
2019-09-09 13:29:03 -0500

    starting

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,15 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-algolia`,
+      options: {
+        appId: process.env.GATSBY_ALGOLIA_APP_ID,
+        apiKey: process.env.ALGOLIA_ADMIN_KEY,
+        //queries,
+        chunkSize: 10000, // default: 1000
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/content/blog`,
